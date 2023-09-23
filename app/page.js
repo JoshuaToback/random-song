a0dcb2e20711482dffe94a9ff6a201c08178f97b
JoshuaToback <joshuatoback@gmail.com>
2023-09-23 19:14:53 -0400

    Added refresh button

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import React from "react";
+'use client'
 import RandomSong from "./components/RandomSong";
 
 // Import date-fns for date and time formatting
@@ -8,6 +8,11 @@ const HomePage = () => {
   // Get the current date and time
   const currentDate = new Date();
 
+  const handleRefresh = () => {
+    // Reload the page
+    window.location.reload();
+  };
+
   return (
     <div className="container mx-auto p-4">
       {/* Stylized date and time */}
@@ -16,10 +21,19 @@ const HomePage = () => {
         {format(currentDate, "MMMM d, yyyy h:mm a")}
       </div>
       {/* Apply the gradient text class to the h1 */}
-      <h1 className={`text-4xl text-center green_gradient my-6 font-semibold green_gradient`}>
+      <h1 className="text-4xl text-center green_gradient my-6 font-semibold">
         Random Song
       </h1>
       <RandomSong />
+      {/* Center align the button */}
+      <div className="flex justify-center">
+        <button
+          className="bg-green-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded mt-2"
+          onClick={handleRefresh}
+        >
+          Refresh
+        </button>
+      </div>
     </div>
   );
 };
